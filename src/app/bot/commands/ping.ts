2b4a69ff6592516fd713a4f6c073345d98143588
iRedSC <redstonecharger@gmail.com>
2025-10-26 22:09:05 -0400

    Set up basic bot functionality

---
+++ src/app/bot/commands/ping.ts
@@ -0,0 +1,13 @@
+import type { Command } from "@definitions/command";
+
+import { SlashCommandBuilder } from "discord.js";
+
+export const command: Command = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
+};
---
+++ src/app/bot/register_commands.ts
@@ -0,0 +1,29 @@
+import { REST, Routes } from "discord.js";
+import { readdirSync } from "fs";
+import path from "path";
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = readdirSync(commandsPath).filter((f) => f.endsWith(".ts"));
+
+const commands = commandFiles.map((file) => {
+  const { command } = require(path.join(commandsPath, file));
+  return command.data.toJSON();
+});
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
+
+(async () => {
+  try {
+    console.log("Registering application commands...");
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID!,
+        process.env.GUILD_ID!
+      ),
+      { body: commands }
+    );
+    console.log("Commands registered.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
