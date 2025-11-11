35a98aea2608bad100ba468c82978245f9e4b8d8
iRedSC <redstonecharger@gmail.com>
2025-11-11 17:22:26 -0500

    added defer reply

--- src/app/bot/commands/gold.ts
+++ src/app/bot/commands/gold.ts
@@ -31,6 +31,7 @@ export const command: Command = {
 
   async execute(interaction) {
     const username = interaction.user.globalName
+    await interaction.deferReply()
 
     const sub = interaction.options.getSubcommand();
     const discordId = interaction.user.id;
